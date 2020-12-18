358b67d6b614dfb19b6b151f7a0235087f08384c
Joel Medina <medina.joel1997@gmail.com>
2020-12-18 09:57:45 +0100

    Bucles

---
+++ Potencia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FonamentsCSharp
+{
+    class Potencia
+    {
+        static void Main(string[] args){
+
+            int b = 0;
+            int n = 0;
+            int result = 1;
+
+            Console.WriteLine("Entra el número de base");
+            int.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("Entra la potencia");
+            int.TryParse(Console.ReadLine(), out n);
+
+            for(int i = 0; i < n; i++){
+                result *= b;
+            }
+
+            Console.WriteLine("Resultado de la potencia " + b + "^" + n + " : " + result);
+        }
+    }
+}

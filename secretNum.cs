356b8cc76b3a26059e2cce826fdb5a131ca106c0
Joel Medina <medina.joel1997@gmail.com>
2020-12-18 09:46:41 +0100

    Secret Num

--- secretNum.cs
+++ secretNum.cs
@@ -7,7 +7,30 @@ namespace FonamentsCSharp
     class secretNum
     {
         static void Main(string[] args){
+            //RANDOM NUMBER
+            Random rd = new Random();
+            int numSecret = rd.Next(1, 5);
+            Console.WriteLine(numSecret);
 
+            for(int i = 0; i<3; i++)
+            {
+                int num = 0;
+                Console.WriteLine("Intento " + (i + 1) + ":");
+                int.TryParse(Console.ReadLine(), out num);
+
+                if(num == numSecret){
+                    Console.WriteLine("Acertaste!!!!");
+                    break;
+                }
+                else if(i == 2){
+                    Console.WriteLine("HAS PERDIDO");
+                }
+                else
+                {
+
+                    Console.WriteLine("Prueba otra vez, fallaste");
+                }
+            }
         }
     }
 }

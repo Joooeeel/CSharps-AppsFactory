7107bb30fb9c1686a2fd6059c7002fa6c163f7cb
Joel Medina <medina.joel1997@gmail.com>
2020-12-18 11:26:31 +0100

    Bucles

---
+++ factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FonamentsCSharp
+{
+    class factorial
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            double result = 1;
+            Console.WriteLine("Entra un numero");
+            int.TryParse(Console.ReadLine(), out num);
+
+            for(int i = num; i > 0; i--){
+                result *= i;
+            }
+
+            Console.WriteLine("Factorial de: !" + num + "= " + result);
+        }
+    }
+}

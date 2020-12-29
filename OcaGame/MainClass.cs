e8110402e8969ca313c3b8e2b8fd269e6b8ee9cc
Joel Medina <medina.joel1997@gmail.com>
2020-12-29 13:30:33 +0100

    Juego de la oca

---
+++ OcaGame/MainClass.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FonamentsCSharp.OcaGame
+{
+    class MainClass
+    {
+        static void Main(string[] args){
+            //Initialize Random
+            Random r = new Random();
+            
+            //Lista de Jugadores
+            List<Player> players = new List<Player>();
+
+            //DIccionario de ocas
+            Dictionary<int, int> ocas = new Dictionary<int, int>();
+            Dictionary<int, int> puente = new Dictionary<int, int>();
+            Dictionary<int, int> dados = new Dictionary<int, int>();
+            Dictionary<int, int> penalizaciones = new Dictionary<int, int>();
+
+            //Iniciamos la posicion de ocas
+            ocas = init(0);
+            puente = init(1);
+            dados = init(2);
+            penalizaciones = init(3);
+
+            
+            int numPlayers;
+
+            Console.WriteLine("Bienvenido al juego de la OCA");
+            Console.WriteLine("");
+            Console.WriteLine("Introduzca el número de jugadores:");
+            int.TryParse(Console.ReadLine(), out numPlayers);
+
+            for(int i=0; i < numPlayers; i++){
+                players.Add(new Player());
+                Console.WriteLine("Introduza el nombre del jugador: ");
+                players[i].nombre = Console.ReadLine();
+                players[i].casilla = 1;
+                players[i].turnosRestantes = 0;
+                players[i].isActive = true;
+            }
+
+            //GAME START
+            bool isActive = true;
+            int newMove;
+            int player = 0;
+
+            while (isActive){
+                newMove = r.Next(1, 7);
+
+                if(players[player].isActive && players[player].turnosRestantes == 0){
+                    players[player].casilla += newMove;
+                    Console.WriteLine(players[player].nombre + " se ha movido a la casilla: " + players[player].casilla);
+
+                    //CHECK IF POSITION CONTAINS OCA
+                    if (ocas.ContainsKey(players[player].casilla))
+                    {
+                        Console.WriteLine("De oca a oca y tiro porque me toca");
+                        players[player].casilla = ocas[players[player].casilla];
+                        Console.WriteLine(players[player].nombre + " se ha movido a la casilla: " + players[player].casilla);
+                    }
+
+                    //CHECK IF POSITION CONTAINS PUENTE
+                    if (puente.ContainsKey(players[player].casilla))
+                    {
+                        Console.WriteLine("De puente a puente y tiro porque me lleva la corriente");
+                        players[player].casilla = puente[players[player].casilla];
+                        Console.WriteLine(players[player].nombre + " se ha movido a la casilla: " + players[player].casilla);
+                    }
+
+                    //CHECK IF POSITION CONTAINS DADO
+                    if (dados.ContainsKey(players[player].casilla))
+                    {
+                        Console.WriteLine("De dado a dado y tiro porque me ha tocado");
+                        players[player].casilla = dados[players[player].casilla];
+                        Console.WriteLine(players[player].nombre + " se ha movido a la casilla: " + players[player].casilla);
+                    }
+
+                    //CHECK IF POSITION TAKES SOME PENALITATION
+                    if (penalizaciones.ContainsKey(players[player].casilla))
+                    {
+                        players[player].turnosRestantes = penalizaciones[players[player].casilla];
+
+                        if (players[player].turnosRestantes == 10){
+                            players[player].isActive = false;
+                            Console.WriteLine(players[player].nombre + " ha muerto");
+                        }
+
+
+                        Console.WriteLine(players[player].nombre + " tiene una penalización de: " + players[player].turnosRestantes + " turnos.");
+                    }
+                }
+                else{
+                    players[player].turnosRestantes--;
+                }
+
+
+
+                //CHECK IF FINISH
+                /*if (players[player].casilla >= 63){
+                    Console.WriteLine("");
+                    Console.WriteLine("El jugador: " + players[player].nombre + " HA GANADO!!!!!!!! ");
+                    isActive = false;
+                }*/
+
+                if (players[player].casilla == 63)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine(players[player].nombre + " HA GANADO!!!!!!!! ");
+                    isActive = false;
+                }else if(players[player].casilla > 63){
+                    int moveBack = players[player].casilla - 63;
+                    int actual = players[player].casilla - newMove;
+
+                    players[player].casilla = 63 - moveBack;
+                    Console.WriteLine(players[player].nombre + " se ha pasado y retrocede");
+                    Console.WriteLine(players[player].nombre + " se ha movido de: " + actual + " a la casilla: " + players[player].casilla);
+                }
+
+                if (player < numPlayers - 1) player++; else player=0;
+
+
+            }
+            
+
+        }
+
+        static Dictionary<int, int> init(int game){
+            Dictionary<int, int> final = new Dictionary<int, int>();
+
+            if(game == 0){ 
+                final.Add(5, 9);
+                final.Add(9, 14);
+                final.Add(14, 18);
+                final.Add(23, 27);
+                final.Add(27, 32);
+                final.Add(32, 36);
+                final.Add(36, 41);
+                final.Add(41, 45);
+                final.Add(45, 50);
+                final.Add(50, 54);
+                final.Add(54, 59);
+            }else if(game == 1){
+                final.Add(6, 12);
+                final.Add(12, 6);
+            }else if(game == 2){
+                final.Add(26, 53);
+                final.Add(53, 26);
+            }else if(game == 3){
+                final.Add(19, 2);
+                final.Add(3, 1);
+                final.Add(52, 3);
+                final.Add(58, 10);
+            }
+            return final;
+        }
+    }
+}
---
+++ OcaGame/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FonamentsCSharp.OcaGame
+{
+    class Player
+    {
+        string _nombre;
+        int _casilla;
+        int _turnosRestantes;
+        bool _isActive;
+
+        public string nombre{
+            get{ return _nombre;
+            }set{
+                _nombre = value;
+            }
+        }
+        public int casilla{
+            get{
+                return _casilla;
+            } set{
+                _casilla = value;
+            }
+        }
+
+        public int turnosRestantes{
+            get{
+                return _turnosRestantes;
+            }set{
+                _turnosRestantes = value;
+            }
+        }
+
+        public bool isActive{
+            get{
+                return _isActive;
+            }set{
+                _isActive = value;
+            }
+        }
+    }
+}
